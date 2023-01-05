a0100a9600efbe132eef684762457b90591de37b
Simon <baptiste.simon@etudiant.univ-reims.fr>
2023-01-05 17:51:54 +0100

    avancement setContenuCellule

--- Model/Cellule.py
+++ Model/Cellule.py
@@ -4,6 +4,8 @@
 import const
 from Model.Constantes import *
 from Model.Coordonnee import *
+
+
 #
 # Modélisation d'une cellule de la grille d'un démineur
 #
@@ -20,16 +22,39 @@ def type_cellule(cell: dict) -> bool:
         and type(cell[const.VISIBLE] == bool) and type(cell[const.CONTENU]) == int \
         and (0 <= cell[const.CONTENU] <= 8 or cell[const.CONTENU] == const.ID_MINE)
 
-def isContenuCorrect(n : int) -> bool:
+
+def isContenuCorrect(n: int) -> bool:
     r = False
     if type(n) == int:
         r = (n >= 0 and n <= 8) or (n == const.ID_MINE)
     return r
 
 
-def construireCellule(contenu=0, visibilite=False) -> dict:
+
+def construireCellule (contenu = 0, visibilite = False):
     if type(contenu) != int:
-        raise ValueError(f"construireCellule : le contenu {contenu} n'est pas correct")
+        raise ValueError(f"construireCellule : le contenu ({contenu}) n'est pas correct")
     if type(visibilite) != bool:
-        raise TypeError(f"construireCellule : le second paramètre {type(visibilite)} n'est pas un booléen")
-    return {contenu:visibilite}
+        raise TypeError(f"construireCellule : le second paramètre ({type(visibilite)}) n'est pas un booléen")
+    return {const.CONTENU : contenu, const.VISIBLE : visibilite}
+
+def getContenuCellule(cellule:dict) -> int:
+    if type_cellule(cellule) == False :
+        raise TypeError("getContenuCellule : Le paramètre n’est pas une cellule.")
+    return cellule[const.CONTENU]
+
+def isVisibleCellule(cellule:dict) -> bool:
+    if type_cellule(cellule) == False :
+        raise TypeError("isVisibleCellule : Le paramètre n’est pas une cellule.")
+    return cellule[const.VISIBLE]
+
+
+def setContenuCellule(cellule:dict,contenu:int) -> None :
+    if type(contenu) == int:
+        raise TypeError("setContenuCellule : Le second paramètre n’est pas un entier.")
+    if not ((contenu >= 0 and contenu <= 8) or (contenu == const.ID_MINE)):
+        raise ValueError(f"setContenuCellule : la valeur du contenu ({contenu}) n’est pas correcte.")
+    if type_cellule(cellule) == False:
+        raise TypeError("setContenuCellule : Le premier paramètre n’est pas une cellule.")
+    cellule[const.CONTENU] = contenu
+    return None
